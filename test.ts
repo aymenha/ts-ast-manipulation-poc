2c42bf6e18a53580257bc08506f8c9ba46437696
Mohamed Aymen Hammami <45166849+aymenha@users.noreply.github.com>
2022-10-06 13:26:32 +0200

    add test file

---
+++ test.ts
@@ -0,0 +1,12 @@
+import { Component } from "@angular/core";
+
+export const blockslist = [
+  {
+    id: "blockid1",
+    name: "block 1",
+  },
+  {
+    id: "blockid2",
+    name: "block 2",
+  },
+];
